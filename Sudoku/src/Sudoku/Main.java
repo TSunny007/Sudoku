d923afafd6b0974ebd35c97d1981c853f258fb9a
Mark Van der Merwe <vandermerwem2@gmail.com>
2017-01-27 23:23:59 -0700

    Original push with base classes.

---
+++ Sudoku/src/Sudoku/Main.java
@@ -0,0 +1,20 @@
+/**
+ * Mark Van der Merwe and Tarun Sunkaraneni
+ */
+package Sudoku;
+
+/**
+ * @author markvandermerwe
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
